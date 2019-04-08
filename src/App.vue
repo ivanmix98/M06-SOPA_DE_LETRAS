78644b89d3e300687b2638611571ec4b04be5de2
ivfosa1819daw2 <ivfosa1819daw2@gmail.com>
2019-04-08 20:32:20 +0200

    Rutas puestas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
+    <router-link to="/"  style="color:#42b983">Home</router-link> |
+    <router-link to="/paraules" style="color:#42b983">Paraules</router-link><br>
     <img src="./assets/logo.png">
     <router-view/>
+
   </div>
 </template>
 
